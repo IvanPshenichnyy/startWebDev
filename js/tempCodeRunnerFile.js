d2b446f371f6dab0396d3b59d5cfc4525cbd7e55
ivan <iv.psheniincome 4nii@yandex.ru>
2019-04-19 17:31:34 +0300

    first commit

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+var money = prompt('Ваш бюджет на месяц?'),
+ time = prompt('Введите дату в формате YYYY-MM-DD', ''),
+ appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        'Введите обязательную статью расходов в этом месяце' : 'Во сколько обойдется?'
+    },
+    optionalExpenses: {},
+    income: [],
+    saving: false
+
+};
+
+    console.log(appData);
